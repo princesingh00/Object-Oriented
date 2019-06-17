d57207fd519be02aff8fcae333a62cb4923c5551
prince singh <singhprince0088>
2019-06-17 16:57:44 +0530

    Stock Management

---
+++ Stock_Program.java
@@ -0,0 +1,60 @@
+package com.bridgeit.objectoriented;
+
+public class Stock_Program {
+
+	private String name;
+	private String company;
+	private String symbol;
+	private int shares;
+	private int price;
+	private int total;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public int getShares() {
+		return shares;
+	}
+
+	public void setShares(int shares) {
+		this.shares = shares;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+}
